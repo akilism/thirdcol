d64e1d303960905808e82a91d9c1867768e9b7be
Brian Kim <brian.kim@vice.com>
2015-10-19 11:42:46 -0400

    add fonts

--- scripts/client.js
+++ scripts/client.js
@@ -4,5 +4,6 @@ import {default as ReactDOM} from "react-dom";
 import {default as ReactRoot} from "./ReactRoot";
 
 import {} from "../styles/reset.css";
+import {} from "../styles/fonts.css";
 
 ReactDOM.render(<ReactRoot />, document.getElementById("react-container"));
